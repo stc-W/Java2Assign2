d2d726c62a5efdd8e358deefe07f81e086c93c96
Wang Hailong <2905219543@qq.com>
2022-11-22 20:02:04 +0800

    first

--- GameClient2.java
+++ GameClient2.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ConnectException;
@@ -20,9 +19,11 @@ public class GameClient2 extends Application {
     private boolean waiting = true;
     Boolean start = false;
     GameInterface gameInterface;
+    
     public static void main(String[] args) throws IOException {
         Application.launch(args);
     }
+    
     @Override
     public void start(Stage stage) throws Exception {
         final int GAME_PORT = 4399;
@@ -33,7 +34,7 @@ public class GameClient2 extends Application {
         stage.setScene(scene);
         stage.show();
         try {
-            Socket s = new Socket("localhost",  GAME_PORT);
+            Socket s = new Socket("localhost", GAME_PORT);
             in = new Scanner(s.getInputStream());
             out = new PrintWriter(s.getOutputStream());
             stage.setOnCloseRequest(e -> {
@@ -47,22 +48,24 @@ public class GameClient2 extends Application {
             stage.close();
             System.out.println("The server has disconnected");
             System.exit(1);
-        }gameInterface.chessBoard.setOnMouseClicked(e -> {
-            if(e.getX() > 48 && e.getX() < 198 && e.getY() > 2 && e.getY() < 152 && currentPlayer[0] == player && start) {
-                selectX = (int)((e.getX() - 48) / 51);
-                selectY = (int)((e.getY() - 2) / 51);
+        }
+        gameInterface.chessBoard.setOnMouseClicked(e -> {
+            if (e.getX() > 48 && e.getX() < 198 && e.getY() > 2 && e.getY()
+              < 152 && currentPlayer[0] == player && start) {
+                selectX = (int) ((e.getX() - 48) / 51);
+                selectY = (int) ((e.getY() - 2) / 51);
                 setChess(selectX, selectY, currentPlayer);
                 waiting = false;
             }
         });
         new Thread(() -> {
-                player = Integer.parseInt(in.nextLine());
-                if (player == 1) {
-                    System.out.println(in.nextLine());
-                }
+            player = Integer.parseInt(in.nextLine());
+            if (player == 1) {
                 System.out.println(in.nextLine());
-                 start = true;
-                while (true) {
+            }
+            System.out.println(in.nextLine());
+            start = true;
+            while (true) {
                 if (player == 1) {
                     try {
                         waitForPlayer();
@@ -83,42 +86,47 @@ public class GameClient2 extends Application {
             }
         }).start();
     }
+    
     public void sendMove() {
         out.println(selectX);
         out.flush();
         out.println(selectY);
         out.flush();
     }
+    
     public void waitForPlayer() throws InterruptedException {
         while (waiting) {
             Thread.sleep(100);
         }
         waiting = true;
     }
+    
     public void receiveMove(Stage stage) {
         if (!gameInterface.gameOver) {
-        try {
-            int x = in.nextInt();
-            if (x == -1) {
-                System.out.println("Another player quit the game");
+            try {
+                int x = in.nextInt();
+                if (x == -1) {
+                    System.out.println("Another player quit the game");
+                    Platform.runLater(stage::close);
+                    System.exit(1);
+                }
+                int y = in.nextInt();
+                Platform.runLater(() -> setChess(x, y, currentPlayer));
+            } catch (NoSuchElementException e) {
                 Platform.runLater(stage::close);
+                System.out.println("The server has disconnected");
                 System.exit(1);
             }
-            int y = in.nextInt();
-            Platform.runLater(() -> setChess(x, y, currentPlayer));
-        } catch (NoSuchElementException e) {
-            Platform.runLater(stage::close);
-            System.out.println("The server has disconnected");
-            System.exit(1);
-        }
         }
     }
+    
     public void setChess(int x, int y, int[] currentPlayer) {
         double x1 = 48 + x * 51;
         double y1 = 2 + y * 51;
         double x2 = 48 + (x + 1) * 51;
         double y2 = 2 + (y + 1) * 51;
-        if (currentPlayer[0] == 1 && !gameInterface.gameOver && gameInterface.chessBoard.chessBoard[x][y] == 0) {
+        if (currentPlayer[0] == 1 && !gameInterface.gameOver
+          && gameInterface.chessBoard.chessBoard[x][y] == 0) {
             gameInterface.chessBoard.chessBoard[x][y] = currentPlayer[0];
             gameInterface.chessBoard.getChildren().add(new CirclePane(x1 + 25.5, y1 + 25.5));
             gameInterface.topStackPane.getChildren().clear();
@@ -132,10 +140,10 @@ public class GameClient2 extends Application {
         if (CheckWin.Check(gameInterface.chessBoard.chessBoard) == 2) {
             gameInterface.gameOver = true;
             gameInterface.setBottom(gameInterface.XWin);
-        }else if (CheckWin.Check(gameInterface.chessBoard.chessBoard) == 1){
+        } else if (CheckWin.Check(gameInterface.chessBoard.chessBoard) == 1) {
             gameInterface.gameOver = true;
             gameInterface.setBottom(gameInterface.OWin);
-        }else if (CheckWin.Check(gameInterface.chessBoard.chessBoard) == 0) {
+        } else if (CheckWin.Check(gameInterface.chessBoard.chessBoard) == 0) {
             gameInterface.gameOver = true;
             gameInterface.setBottom(gameInterface.Draw);
         }
--- GameInterface.java
+++ GameInterface.java
@@ -1,43 +1,37 @@
-
 import javafx.geometry.Pos;
 import javafx.scene.layout.BorderPane;
-
 import javafx.scene.layout.StackPane;
-
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
-
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.paint.Color;
-
-
 import java.io.FileNotFoundException;
 
 public class GameInterface extends BorderPane {
-    int player = 1;
-    boolean gameOver = false;
-    ChessBoard chessBoard = new ChessBoard();
-    StackPane topStackPane = new StackPane();
-    Text playerO = new Text(10, 10, "It's O-Player's turn.");
-    Text playerX = new Text(10, 10, "It's X-Player's turn.");
-    Text XWin = new Text(10, 10, "Game over, winner is X-Player");
-    Text OWin = new Text(10, 10, "Game over, winner is O-Player");
-    Text Draw = new Text(10, 10, "Game over, there is no winner");
-    public GameInterface() {
-        setAlignment(XWin, Pos.TOP_CENTER);
-        setAlignment(OWin, Pos.TOP_CENTER);
-        setAlignment(Draw, Pos.TOP_CENTER);
-        setAlignment(chessBoard, Pos.CENTER);
-        setAlignment(topStackPane, Pos.TOP_CENTER);
-        playerO.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
-        playerX.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
-        XWin.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
-        OWin.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
-        topStackPane.getChildren().add(playerO);
-        setCenter(chessBoard);
-        setTop(topStackPane);
-    }
+  int player = 1;
+  boolean gameOver = false;
+  ChessBoard chessBoard = new ChessBoard();
+  StackPane topStackPane = new StackPane();
+  Text playerO = new Text(10, 10, "It's O-Player's turn.");
+  Text playerX = new Text(10, 10, "It's X-Player's turn.");
+  Text XWin = new Text(10, 10, "Game over, winner is X-Player");
+  Text OWin = new Text(10, 10, "Game over, winner is O-Player");
+  Text Draw = new Text(10, 10, "Game over, there is no winner");
+  public GameInterface() {
+    setAlignment(XWin, Pos.TOP_CENTER);
+    setAlignment(OWin, Pos.TOP_CENTER);
+    setAlignment(Draw, Pos.TOP_CENTER);
+    setAlignment(chessBoard, Pos.CENTER);
+    setAlignment(topStackPane, Pos.TOP_CENTER);
+    playerO.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
+    playerX.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
+    XWin.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
+    OWin.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
+    topStackPane.getChildren().add(playerO);
+    setCenter(chessBoard);
+    setTop(topStackPane);
+  }
 }
